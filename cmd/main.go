1cad7f0ea515b23a96290c2584c578412478ece3
NamozovAzizbek <salom_007@proton.me>
2023-04-20 11:06:25 +0500

    grab

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,17 @@
-package cmd
+package main
 
 import (
 	"log"
+	"osd/pkg/authz"
+	modul "osd/pkg/module"
+	"osd/pkg/server"
 
-	
 	"github.com/casbin/casbin"
+	"github.com/gorilla/mux"
 )
 
 type authorizer struct {
-	users    module.Users
+	users    modul.Users
 	enforcer *casbin.Enforcer
 }
 
@@ -27,4 +30,24 @@ func (a *authorizer) HasPermission(userID, action, asset string) bool {
 	}
 
 	return false
+}
+
+func main() {
+	enforcer, err := casbin.NewEnforcerSafe("../casbin.conf", "../rbac_policy.csv")
+	if err != nil {
+		log.Fatal("Failed to create enforcer:", err)
+	}
+
+	users, err := modul.Load()
+	if err != nil {
+		log.Fatal("Failed to load users:", err)
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/{asset}", server.Handler).Methods("GET", "POST", "DELETE")
+	router.Use(
+		authz.Middleware(&authorizer{users: users, enforcer: enforcer}),
+	)
+
+	server.Start(router)
 }
