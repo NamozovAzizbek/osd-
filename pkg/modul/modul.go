89f8babb959d4616016c84e30189b11ed3414e5d
NamozovAzizbek <salom_007@proton.me>
2023-04-20 10:57:20 +0500

    modul

--- pkg/modul/modul.go
+++ pkg/modul/modul.go
@@ -1,1 +1,25 @@
 package modul
+
+import "osd/pkg/file"
+
+type User struct {
+	ID    string   `json:"id"`
+	Roles []string `json:"roles"`
+}
+
+type Users map[string]User
+
+func Load() (Users, error) {
+	var userList []User
+
+	if err := file.LoadJson("../users.json", &userList); err != nil {
+		return nil, err
+	}
+
+	users := Users{}
+	for _, user := range userList {
+		users[user.ID] = user
+	}
+
+	return users, nil
+}
